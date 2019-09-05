a083261bb52b2581c4a0b6eec3e79f7c60e4a4a7
RuninngDaYuer <18815610927@163.com>
2019-09-05 20:47:17 +0800

    add index and bootstrap

--- src/main/java/combpddyer/cmmunity1/controller/IndexController.java
+++ src/main/java/combpddyer/cmmunity1/controller/IndexController.java
@@ -13,10 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 
 @Controller
-public class HelloController {
-    @GetMapping
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
